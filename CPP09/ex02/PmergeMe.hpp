09b00824809512f16e8681e7f75a59e97baec67d
rel-isma <relismaiyly@gmail.com>
2024-02-09 19:05:23 +0100

    work in cpp09 in ex01

---
+++ CPP09/ex02/PmergeMe.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-isma <rel-isma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 16:38:01 by rel-isma          #+#    #+#             */
+/*   Updated: 2024/02/09 16:39:29 by rel-isma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+class PmergeMe
+{
+    private:
+        /* data */
+    public:
+        PmergeMe(/* args */);
+        ~PmergeMe();
+};
+
+#endif // PMERGEME_HPP
